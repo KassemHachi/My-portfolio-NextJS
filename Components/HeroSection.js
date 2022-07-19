2db2bdd6b869d01bfdcef0e2cb49fef5fa0e2441
kassem hachi <kassem.hachi@outlook.com>
2022-07-19 11:15:59 +0100

    Add UserCard Component

--- Components/HeroSection.js
+++ Components/HeroSection.js
@@ -1,7 +1,5 @@
 
-import TwitterLink from "../Components/TwitterLink";
-import GithubLink from "../Components/GithubLink";
-import FreelancerLink from "../Components/FreelancerLink";
+import UserCard from "./UserCard";
 export default function HeroSection() {
   return (
     <div className=" content-center mx-auto justify-center top-full relative">
@@ -14,30 +12,8 @@ export default function HeroSection() {
         className="z-0  h-screen w-full absolute top-0 bg-gradient-to-t from-slate-800 to-slate-800/80"
 
       ></div>
+      <UserCard />
 
-      <div className="z-10 absolute top-44 md:right-[42.5%] right-[25%] sm:right-[30%] animate-fade-in-up">
-
-        <div className="border-4 border-green-400/80 rounded-full overflow-clip shadow-xl">
-
-
-          <img
-            className="h-56 w-56 z-20"
-            src="https://avatars.githubusercontent.com/u/30910460?v=4"
-          />
-          <span className="animate-ping absolute top-0 inline-flex h-56 w-56  rounded-full bg-green-500 opacity-75 -z-10"></span>
-        </div>
-        <div className="bg-slate-800 -z-20 absolute right-0 left-[-15px] top-[10rem] text-center text-slate-100 rounded-lg shadow-xl h-72 w-64">
-          <h1 className="text-3xl font-bold mt-28  ">Kassem Hachi</h1>
-          <p className=" text-sm pb-6 mb-10 border-b-2 border-green-400/80 text-slate-100/70">
-            Programmer, Designer, Animator
-          </p>
-          <div className="flex justify-center items-center gap-4">
-            <GithubLink href="https://github.com/KassemHachi/" />
-            <TwitterLink href="https://twitter.com/kassemhachi" />
-            <FreelancerLink href="https://www.freelancer.com/u/kassemhachi" />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- Components/ProjectsSection.js
+++ Components/ProjectsSection.js
@@ -22,25 +22,25 @@ export default function ProjectsSection() {
 
 
         <div className="rounded-xl h-64 w-full md:w-64 bg-gray-800 mx-3 my-3 overflow-clip cursor-pointer ">
-        <ModalView 
-        Title ="Wifi Djelfa"
-        Small="https://scontent.floo1-1.fna.fbcdn.net/v/t1.15752-9/285612037_437229055075313_2048625068671358490_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGrzTD-AdcFo2Sk19knG-4DfJfUQqjbLm98l9RCqNsub4ACOwDkMO9svScBW20Y7NliCB0gjBR02UYvlUokO_uc&_nc_ohc=XA9YvoR2zp4AX-L_miv&_nc_oc=AQmN87EVVnk42ASQjAbqzzHvmdUsnlTg2Xv7L0GgbsRu6W1U_rYXGHmogf8QBdsEQWw&_nc_ht=scontent.floo1-1.fna&oh=03_AVL83ij3PP0wlBjn8N-6JxVX-2adwwwCoBloOLtgKq5yVQ&oe=62DC7C29" 
-        Large="https://i.suar.me/eWeYK/l"
-        />
-       </div>
+          <ModalView
+            Title="Wifi Djelfa"
+            Small="https://scontent.floo1-1.fna.fbcdn.net/v/t1.15752-9/285612037_437229055075313_2048625068671358490_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGrzTD-AdcFo2Sk19knG-4DfJfUQqjbLm98l9RCqNsub4ACOwDkMO9svScBW20Y7NliCB0gjBR02UYvlUokO_uc&_nc_ohc=XA9YvoR2zp4AX-L_miv&_nc_oc=AQmN87EVVnk42ASQjAbqzzHvmdUsnlTg2Xv7L0GgbsRu6W1U_rYXGHmogf8QBdsEQWw&_nc_ht=scontent.floo1-1.fna&oh=03_AVL83ij3PP0wlBjn8N-6JxVX-2adwwwCoBloOLtgKq5yVQ&oe=62DC7C29"
+            Large="https://i.suar.me/eWeYK/l"
+          />
+        </div>
         <div className="rounded-xl h-64 w-full md:w-64 bg-gray-800 mx-3 my-3  overflow-clip cursor-pointer ">
-        <ModalView 
-        Title ="Terrain Metrcis Hub"
-        Small="https://scontent.floo1-2.fna.fbcdn.net/v/t1.15752-9/288854732_709107717050633_8949983914437839660_n.png?_nc_cat=110&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGd9YnNg-zm_VNlf9LEetnXyCZk4yI_j7rIJmTjIj-PukZnTYyUl2yvoik7_Y6YR_0Ovt4MoHc9mqHZnB6KnWCL&_nc_ohc=m8VkrVwMN8cAX9cHw2M&_nc_ht=scontent.floo1-2.fna&oh=03_AVJsXAbe0-l3342inkmr0ROab4fjaRiJBjZZn2VM0108kw&oe=62DF9542" 
-        Large="https://i.suar.me/pyW22/l"
-        />
+          <ModalView
+            Title="Terrain Metrcis Hub"
+            Small="https://scontent.floo1-2.fna.fbcdn.net/v/t1.15752-9/288854732_709107717050633_8949983914437839660_n.png?_nc_cat=110&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGd9YnNg-zm_VNlf9LEetnXyCZk4yI_j7rIJmTjIj-PukZnTYyUl2yvoik7_Y6YR_0Ovt4MoHc9mqHZnB6KnWCL&_nc_ohc=m8VkrVwMN8cAX9cHw2M&_nc_ht=scontent.floo1-2.fna&oh=03_AVJsXAbe0-l3342inkmr0ROab4fjaRiJBjZZn2VM0108kw&oe=62DF9542"
+            Large="https://i.suar.me/pyW22/l"
+          />
         </div>
         <div className="rounded-xl h-64 w-full md:w-64 bg-gray-800 mx-3 my-3   overflow-clip cursor-pointer">
-        <ModalView 
-        Title ="App Promo Page"
-        Small="https://scontent.floo1-2.fna.fbcdn.net/v/t1.15752-9/281869547_408963854596991_6671964074615137119_n.png?_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeF_XyIj0HaI6Jh25Fmq1GR6Jl8iJH8s9Z0mXyIkfyz1nW4trrLde1-yvrqsDujzDY1nwvfq8QUcYY83dQIe-m9T&_nc_ohc=Kf4hZE0qHcQAX_hjPgX&_nc_ht=scontent.floo1-2.fna&oh=03_AVIazG0OiOjDaIcNHkyPAiiCLvXZOEoSWSzN9VajQZTRDA&oe=62DCF537" 
-        Large="https://i.suar.me/oK4w1/l"
-        />
+          <ModalView
+            Title="App Promo Page"
+            Small="https://scontent.floo1-2.fna.fbcdn.net/v/t1.15752-9/281869547_408963854596991_6671964074615137119_n.png?_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeF_XyIj0HaI6Jh25Fmq1GR6Jl8iJH8s9Z0mXyIkfyz1nW4trrLde1-yvrqsDujzDY1nwvfq8QUcYY83dQIe-m9T&_nc_ohc=Kf4hZE0qHcQAX_hjPgX&_nc_ht=scontent.floo1-2.fna&oh=03_AVIazG0OiOjDaIcNHkyPAiiCLvXZOEoSWSzN9VajQZTRDA&oe=62DCF537"
+            Large="https://i.suar.me/oK4w1/l"
+          />
         </div>
         <div className="rounded-xl h-64 w-full md:w-64 bg-gray-800 mx-3 my-3  "></div>
         <div className="rounded-xl h-64 w-full md:w-64 bg-gray-800 mx-3 my-3  "></div>
